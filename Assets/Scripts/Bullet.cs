539bf79d9b291ab730922138b4d8bc3ad4e69f89
Yoel Mekbeb <mekbe003@umn.edu>
2025-01-01 14:01:38 -0600

    Bullet behavior

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 5f;
+    Rigidbody2D bullet;
+    PlayerMovement player;
+    
+    float xSpeed;
+    void Start()
+    {
+        bullet = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovement>();
+        xSpeed = player.transform.localScale.x * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bullet.velocity = new Vector2(xSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Enemy"){
+            Destroy(other.gameObject);
+            
+        }
+        Destroy(gameObject);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll){
+        Destroy(gameObject);
+    }
+}
